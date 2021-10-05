2f7bd0d7c7c7964a794f7531f8319fc2f087f870
ismail codar <icodar@gmail.com>
2021-10-05 19:23:46 +0300

    format & try catch on fn call

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,25 +1,35 @@
-import klona from 'klona';
+import klona from "klona";
 
 function loop(list, data, state, idx, isAsync) {
-	var tmp, fn = list[idx++];
-	if (!fn) return isAsync ? Promise.resolve(state): state;
+	var tmp,
+		fn = list[idx++];
+	if (!fn) return isAsync ? Promise.resolve(state) : state;
 
-	tmp = fn(state, data);
+	try {
+		tmp = fn(data, state);
+	} catch (err) {
+		if (isAsync) return Promise.reject(err);
+		else throw err;
+	}
 	if (tmp == null) return loop(list, data, state, idx);
-	if (typeof tmp.then == 'function') return tmp.then(d => loop(list, data, d, idx, true));
+	if (typeof tmp.then == "function")
+		return tmp.then((d) => loop(list, data, d, idx, true));
 
-	if (typeof tmp == 'object') state = tmp;
+	if (typeof tmp == "object") state = tmp;
 	return loop(list, data, state, idx);
 }
 
 export default function (obj) {
-	var $, tree={}, hooks={}, value = obj || {};
+	var $,
+		tree = {},
+		hooks = {},
+		value = obj || {};
 
 	var rem = (arr, func) => {
 		arr.splice(arr.indexOf(func) >>> 0, 1);
-	}
+	};
 
-	return $ = {
+	return ($ = {
 		get state() {
 			return klona(value);
 		},
@@ -30,22 +40,31 @@ export default function (obj) {
 		},
 
 		set(obj, evt) {
-			return loop((hooks['*'] || []).concat(evt && hooks[evt] || []), value, klona(value = obj), 0);
+			return loop(
+				(hooks["*"] || []).concat((evt && hooks[evt]) || []),
+				value,
+				klona((value = obj)),
+				0
+			);
 		},
 
 		listen(evt, func) {
-			if (typeof evt == 'function') {func=evt; evt='*'}
+			if (typeof evt == "function") {
+				func = evt;
+				evt = "*";
+			}
 			hooks[evt] = (hooks[evt] || []).concat(func);
 			return () => rem(hooks[evt], func);
 		},
 
 		dispatch(evt, data) {
 			var tmp = loop(tree[evt] || [], data, klona(value), 0);
-			if (typeof tmp.then == 'function') return tmp.then(x => {
-				if (x == null) throw 'state did not returned!';
-				return $.set(x, evt);
-			});
+			if (typeof tmp.then == "function")
+				return tmp.then((x) => {
+					if (x == null) throw "state did not returned!";
+					return $.set(x, evt);
+				});
 			else return $.set(tmp, evt);
-		}
-	};
+		},
+	});
 }
